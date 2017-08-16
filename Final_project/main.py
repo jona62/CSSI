fdfe43cc6548d25e80b759e68eff7b956d7b571e
jona62 <jonathanjamesm66@gmail.com>
2017-08-16 16:14:45 -0400

    Project

--- Final_project/main.py
+++ Final_project/main.py
@@ -1,24 +1,68 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.ext import ndb
+import logging
 
-JINJA_ENVIRONMENT = jinja2.Environment(
+ENV = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+class Comment(ndb.Model):
+    title = ndb.StringProperty()
+    text_field = ndb.TextProperty()
+    created_at = ndb.DateTimeProperty( auto_now_add=True )
+
+class FormHandler(webapp2.RequestHandler):
+    def get(self):
+        #self.response.headers['Refresh'] = '0;url="/"'
+        comments = comment.query().order(-Comment.created_at).fetch()
+        template_values = {'name' : 'Anonymous', 'comments' : comments}
+        template = ENV.get_template('/templates/no.1.html')
+        self.response.write(template.render(template_values))
+
+    def post(self):
+        form_title = self.request.get('title')
+        form_content = self.request.get('text_field')
+
+        new_comment = Comment( parent = ndb.Key(Comment, 'dummy'), title = form_title, text_field = form_content)
+        new_comment.put()
+
+        comments = Comment.query(ancestor = ndb.Key(Comment, 'dummy')).order(-Comment.created_at).fetch()
+        template_values = {'name' : 'Anonymous', 'comments' : comments}
+        template = ENV.get_template('/templates/no.1.html')
+        self.response.write(template.render(template_values))
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        no_one= JINJA_ENVIRONMENT.get_template("templates/no.1.html")
-        self.response.write(no_one.render())
+        main= ENV.get_template("/templates/final_project.html")
+        self.response.write(main.render())
+
+class topicHandler(webapp2.RequestHandler):
+    def get(self):
+        topic= ENV.get_template("/templates/topic.html")
+        self.response.write(topic.render())
+
+class no1Handler(webapp2.RequestHandler):
+    def get(self):
+        comments = Comment.query().order(-Comment.created_at).fetch()
+        template_values = {'name' : 'Anonymous', 'comments' : comments}
+        template = ENV.get_template('/templates/no.1.html')
+        self.response.write(template.render(template_values))
+
     def post(self):
-        name = self.request.get("name")
-        text_field = self.request.get("text_field")
-        output = JINJA_ENVIRONMENT.get_template("templates/no.2.html")
-        collective = {'name': name, 'text_field' : text_field}
-        self.response.write(output.render(collective))
+        self.response.write("NO SOUP FOR YOU")
+        # comments = Comment.query().order(-Comment.created_at).fetch()
+        # name = self.request.get("name")
+        # text_field = self.request.get("text_field")
+        # template = ENV.get_template("/templates/no.1.html")
+        # template_values = {'name': name, 'text_field' : text_field}
+        # self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-
+    ('/topic', topicHandler),
+    ('/no1', no1Handler),
+    ('/processform', FormHandler),
 ], debug=True)
--- Final_project/scripts/script.js
+++ Final_project/scripts/script.js
@@ -1,3 +1,17 @@
-//$("#x_btn").click(function(){
-console.log("Hello World!");
-//});
+/*var filter = ["ass", "piss"];
+var comment = document.getElementById("#comment-post-text").innerHTML;
+
+  for (var i=0; i<filter.length; i++){
+    var filter_word = filter[i];
+    var replace = '*'.repeat (filter_word.length);
+  }
+*/
+$(document).ready(function(){
+    $("button").click(function(){
+        var div = $("div");
+        div.animate({height: '300px', opacity: '0.4'}, "slow");
+        div.animate({width: '300px', opacity: '0.8'}, "slow");
+        div.animate({height: '100px', opacity: '0.4'}, "slow");
+        div.animate({width: '100px', opacity: '0.8'}, "slow");
+    });
+});
